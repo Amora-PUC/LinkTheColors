fe622d420b02459a8c0e72818e218080d7465144
Amora <amorauwu2024@gmail.com>
2025-10-28 08:33:33 -0300

    Criação Package Fases e Classe Traço

---
+++ Fases/Fase1.java
@@ -0,0 +1,33 @@
+package Fases;
+/**
+ * Escreva uma descrição da classe Fase1 aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Fase1
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int x;
+
+    /**
+     * Construtor para objetos da classe Fase1
+     */
+    public Fase1()
+    {
+        // inicializa variáveis de instância
+        x = 0;
+    }
+
+    /**
+     * Um exemplo de um método - substitua este comentário pelo seu próprio
+     * 
+     * @param  y   um exemplo de um parâmetro de método
+     * @return     a soma de x e y 
+     */
+    public int sampleMethod(int y)
+    {
+        // escreva seu código aqui
+        return x + y;
+    }
+}
